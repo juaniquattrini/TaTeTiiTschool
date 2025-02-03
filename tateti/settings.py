0f1dfcade8ce69ef48b009c15fe0268b04a2521c
juaniquattrini <juaniquattrini@gmail.com>
2025-02-03 00:29:29 -0300

    Revert "Update settings.py"
    This reverts commit bfc2762e75af03652e68caf5974d1e7ebc9510db.

--- tateti/settings.py
+++ tateti/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'tateti.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
 }
 
 # Password validation
